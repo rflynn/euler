0b22edea67aee7826bc83b14d0b021c009caa431
pizza <pizza@fedora11.(none)>
2009-08-31 10:02:05 -0400

    speed up 12.py factoring by about 5000%; maintain a count instead of a list, since all but one of[C the lists get thrown out anyways. lists must be slow in python, who knew.

--- 12.py
+++ 12.py
@@ -34,27 +34,41 @@ def factors(n):
   f.sort()
   return f
 
+# factor n, maintain a count instead of a list
+def factors2(n):
+  cnt = 2
+  for i in xrange(2, int(math.sqrt(n))+1):
+    if n % i == 0:
+      cnt += 2
+  return cnt
+
 # check factors()
-assert factors(1) == [1,1]
-assert factors(2) == [1,2]
-assert factors(3) == [1,3]
-assert factors(6) == [1,2,3,6]
+assert factors(1)  == [1,1]
+assert factors(2)  == [1,2]
+assert factors(3)  == [1,3]
+assert factors(6)  == [1,2,3,6]
 assert factors(10) == [1,2,5,10]
 assert factors(15) == [1,3,5,15]
 assert factors(21) == [1,3,7,21]
 assert factors(28) == [1,2,4,7,14,28]
 # ensure our sqrt math is right
-assert factors(4) == [1,2,2,4]
+assert factors(4)  == [1,2,2,4]
 assert factors(25) == [1,5,5,25]
+# check others
+assert factors(12) == [1,2,3,4,6,12]
+assert factors(60) == [1,2,3,4,5,6,10,12,15,20,30,60]
+
+print "assertions passed"
 
 tri = 0
 i = 1
-f = []
+cnt = 0
 
-while len(f) <= 500:
+while cnt <= 500:
   tri += i
-  f = factors(tri)
+  cnt = factors2(tri)
   i += 1
 
-print "%12u: (%u factors) %s" % (tri, len(f), ",".join(map(str,f)))
+print "%12u: (%u factors)" % (tri, cnt)
+print factors(tri)
 
